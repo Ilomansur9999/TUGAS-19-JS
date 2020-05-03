ff8082580924fff4d239ff1a7c424126383ad729
ilomansur9999 <ilofaqot@gmail.com>
2020-05-03 21:36:55 +0700

    TUGAS 19 JS
    Tugas ini dikumpulkan ke NIOMIC

---
+++ tugas19.js
@@ -0,0 +1,27 @@
+function repeat(){
+	var isi = "Semangat...";
+	console.log(isi.repeat(5));
+
+	console.log("=============");
+}
+
+
+repeat();
+
+
+
+function repeat1(){
+	var isi = "*****";
+	console.log(isi.repeat(1));
+	console.log(isi.repeat(5));
+	console.log(isi.repeat(10));
+	console.log(isi.repeat(15));
+	console.log(isi.repeat(20));
+	console.log(isi.repeat(15));
+	console.log(isi.repeat(10));
+	console.log(isi.repeat(5));
+	console.log(isi.repeat(1));
+
+}
+
+repeat1();
